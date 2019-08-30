71184fa7711cffc129e8080e2f7afdf2b5cc51d5
michogar <michogar@gmail.com>
2019-08-30 14:05:34 +0200

    v0.2.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='CatastroInspireDownloader',
-    version='0.1.2',
+    version='0.2.0',
     install_requires=[
         'Click==7.0',
         'atoma',
